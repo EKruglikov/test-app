3d6df8672651212e1fadd7c30b61c4ff6e58a408
Egor Alexandrovich <dev.christmas@gmail.com>
2019-03-26 20:54:48 +0300

    Init app

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Test application</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: { }
+}
+</script>
